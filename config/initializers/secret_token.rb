5f50b4d6ebdec564b5934bd5dbc119060ed1fc57
sevendoors <seven5272@gmail.com>
2013-09-06 07:06:02 +0000

    创建基本框架，链接mysql 数据库，创建User脚手架

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offical::Application.config.secret_token = '7f6462d452ed181e568dcd31946faf268b42e7a0be85e0e032d7f0cf59a3c8fdaf1bd52319f7abb6b59310cf3ecae505d97d2b250fde6d8879c92d925e6fe425'
